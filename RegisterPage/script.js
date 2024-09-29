8337e5f2b8df1f882d99b72e7372a66e571455d7
RafaelQueirozViana <rafaelqueirozcomercial3@gmail.com>
2024-09-28 22:50:20 -0300

    Adição de html, css e javascript

---
+++ RegisterPage/script.js
@@ -0,0 +1,17 @@
+const emailInput = document.querySelector("#email-input");
+const atalhoButton = document.querySelectorAll(".button");
+
+
+atalhoButton.forEach(button => {
+    button.addEventListener('click', () => {
+
+        if (!(emailInput.value.includes( button.textContent))) {
+                emailInput.value += button.textContent;
+        }
+
+        
+
+
+
+    });
+})
