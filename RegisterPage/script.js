f50d4a90f36865fa731d7024d403455b98c7ff22
RafaelQueirozViana <rafaelqueirozcomercial3@gmail.com>
2024-10-04 16:16:26 -0300

    html css javascript

--- RegisterPage/script.js
+++ RegisterPage/script.js
@@ -1,17 +1,43 @@
-const emailInput = document.querySelector("#email-input");
-const atalhoButton = document.querySelectorAll(".button");
+function addDomain(domain) {
+    // Pegando o valor atual do campo de email
+    let emailInput = document.getElementById('email-input');
+    let emailValue = emailInput.value;
+
+    // Remover qualquer domínio existente
+    emailValue = emailValue.replace(/@[\w.]+$/, '');
+
+    // Adiciona o novo domínio
+    emailInput.value = emailValue + domain;
+}
 
 
-atalhoButton.forEach(button => {
-    button.addEventListener('click', () => {
 
-        if (!(emailInput.value.includes( button.textContent))) {
-                emailInput.value += button.textContent;
-        }
-
-        
+const passwordIcons = document.querySelectorAll(".eye-icon")
 
 
 
-    });
+passwordIcons.forEach(atualInput => {
+    let stage = "mostrar";
+
+    atualInput.addEventListener('click', function() {
+
+
+    if(stage == "mostrar") {
+        stage = "ocultar"
+        input.type = "text";
+
+
+
+    }
+
+    else if(stage == "ocultar") {
+        stage = "mostrar"
+        alert("ocultu")
+      
+    }
+
+    })
+
 })
+
+
