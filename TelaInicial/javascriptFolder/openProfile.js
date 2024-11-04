2f40c82b8d702b28d55cb81a2ffe3c7db40411d5
RafaelQueirozViana <rafaelqueirozcomercial3@gmail.com>
2024-11-04 13:31:19 -0300

    menu de perfil html css e javascript

---
+++ TelaInicial/javascriptFolder/openProfile.js
@@ -0,0 +1,8 @@
+let accountButton = document.querySelector(".img-button");
+let profileContainer = document.querySelector(".accout-profile");
+
+accountButton.addEventListener("click", function() {
+    console.log("da")
+ profileContainer.classList.toggle("loggedOpen");
+ 
+})
